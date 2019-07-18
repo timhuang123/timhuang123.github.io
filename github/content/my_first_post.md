b8f9f46b75cc60e127c4c646ee5fc2c0489a0115
huangtan <huangtan@huangtans-MacBook-Air.local>
2019-07-19 03:28:53 +0800

    first commit

---
+++ github/content/my_first_post.md
@@ -0,0 +1,12 @@
+Title: My first post
+Date: 2017-11-26 10:01
+Modified: 2017-11-27 12:30
+Category: misc
+Tags: first, misc
+Slug: My-first-post
+Authors: Tim
+Summary: What does your post talk about? Write here.
+
+This is the *first post* from my Pelican blog. **YAY!**
+
+![this is my apple]({apple}/images/apple.jpg)
---
+++ github/pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'tim'
+SITENAME = "Tim's blog"
+SITEURL = ''
+
+PATH = 'Users/huangtan/Desktop/'
+
+TIMEZONE = 'Asia/Chongqing'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('NUS', 'www.nus.edu.sg'))
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
